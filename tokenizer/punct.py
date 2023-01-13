3da7382cd9a4b02f5d480cfc222d71a2739d7a29
lllchak <mediumchak@yandex.ru>
2023-01-13 17:36:32 +0300

    deprecated PunkToken

--- tokenizer/punct.py
+++ tokenizer/punct.py
@@ -4,7 +4,7 @@ import dtypes
 from tokenizer import BaseTokenizer
 
 
-class PunctRegex(BaseTokenizer):
+class PunctTokenizer(BaseTokenizer):
     """
     Stores regular expressions for string tokenization with respect to
     punctuation
@@ -18,7 +18,8 @@ class PunctRegex(BaseTokenizer):
 
     __slots__ = ["_RE_STRING_TOKENIZER"]
 
-    # Format of a regular expression to split punctuation from words 
+    # Format of a regular expression to split punctuation from words
+    # (taken from nltk sources)
     _STRING_TOKENIZER_FMT = r"""(
         %(MultiChar)s
         |
@@ -83,51 +84,54 @@ class PunctRegex(BaseTokenizer):
         Returns:
             List of tokens, i.e "string to tokenize" -> ["string", "to", "tokenize"]
         """
-        
+
         return self.gstring_re.findall(string)
 
 
-class PunctToken:
-    """
-    String (sentence/context) token class. Stores token value, its type
-    and if its final string token flag.
-    """
+"""
+Might be useful in the future, but deprecated for now
+"""
+# class PunctToken:
+#     """
+#     String (sentence/context) token class. Stores token value, its type
+#     and if its final string token flag.
+#     """
         
-    # Numeric token (contains only number(-s))
-    _NUMERIC = re.compile(r"^-?[\.,]?\d[\d,\.-]*\.?$")
-    # Alpha token (contains only alphabetical characters)
-    _ALPHA = re.compile(r"[^\W\d]+$", re.UNICODE)
+#     # Numeric token (contains only number(-s))
+#     _NUMERIC = re.compile(r"^-?[\.,]?\d[\d,\.-]*\.?$")
+#     # Alpha token (contains only alphabetical characters)
+#     _ALPHA = re.compile(r"[^\W\d]+$", re.UNICODE)
 
-    def __init__(self, tk: str) -> None:
-        self.tk = tk
-        self.type = self.__stype(tk)
-        self.tfinal = tk.endswith('.') | \
-                      tk.endswith('?') | \
-                      tk.endswith('!') | \
-                      tk.endswith(';')
+#     def __init__(self, tk: str) -> None:
+#         self.tk = tk
+#         self.type = self.__stype(tk)
+#         self.tfinal = tk.endswith('.') | \
+#                       tk.endswith('?') | \
+#                       tk.endswith('!') | \
+#                       tk.endswith(';')
 
-    def __stype(self, tk) -> None:
-        return self._NUMERIC.sub("NUMERIC", tk.lower())
+#     def __stype(self, tk) -> None:
+#         return self._NUMERIC.sub("NUMERIC", tk.lower())
 
-    @property
-    def tk_no_endchar(self):
-        return self.type[:-1] \
-               if len(self.type) > 1 and self.type[-1] in ['.', '?', '!', "..."] \
-               else self.type
+#     @property
+#     def tk_no_endchar(self):
+#         return self.type[:-1] \
+#                if len(self.type) > 1 and self.type[-1] in ['.', '?', '!', "..."] \
+#                else self.type
 
-    @property
-    def fupper(self) -> bool:
-        return self.tk[0].isupper()
+#     @property
+#     def fupper(self) -> bool:
+#         return self.tk[0].isupper()
 
-    @property
-    def flower(self) -> bool:
-        return self.tk[0].islower()
+#     @property
+#     def flower(self) -> bool:
+#         return self.tk[0].islower()
 
-    @property
-    def is_num(self) -> bool:
-        return self.type.startwith("NUMERIC")
+#     @property
+#     def is_num(self) -> bool:
+#         return self.type.startwith("NUMERIC")
 
-    @property
-    def is_alpha(self) -> bool:
-        return self._ALPHA.match(self.tk)
+#     @property
+#     def is_alpha(self) -> bool:
+#         return self._ALPHA.match(self.tk)
     
