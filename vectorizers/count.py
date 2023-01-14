6cd707565c0ce074029a57160ec4f36ad2444ae6
lllchak <mediumchak@yandex.ru>
2023-01-14 21:32:46 +0300

    CountVectorized docs init

--- vectorizers/count.py
+++ vectorizers/count.py
@@ -27,11 +27,36 @@ class CountVectorizer(BaseVectorizer):
         input: CorpusInput,
         ignore_stopwords: bool = True
     ) -> None:
+        """
+        You can find more complete docs at ./base.py
+
+        Fitting on a given corpus method.
+
+        Args:
+            input (CorpusInput)     : Corpus to fit with
+            ignore_stopwords (bool) : If ignore corpus stopwords or not flag
+
+        Returns:
+            None (only creates corpus vocabulary)
+        """
+
         input = self.__check_input(input)
 
         self.__ccorpus(input=input, ignore_stopwords=ignore_stopwords)
 
     def transform(self, input: CorpusInput) -> VectorizedOutput:
+        """
+        You can find more complete docs at ./base.py
+
+        Tranforming given corpus method.
+
+        Args:
+            input (CorpusInput) : Corpus to be vectorized
+
+        Returns:
+            Vectorized corpus
+        """
+
         input = self.__check_input(input)
 
         return [self.__trsent(sent) for sent in input]
@@ -41,6 +66,19 @@ class CountVectorizer(BaseVectorizer):
         input: CorpusInput,
         ignore_stopwords: bool = True
     ) -> VectorizedOutput:
+        """
+        You can find more complete docs at ./base.py
+
+        Fitting and tranforming corpus wrapper method
+
+        Args:
+            input (CorpusInput)     : Corpus to fit with and which to transform after
+            ignore_stopwords (bool) : If ignore corpus stopwords or not flag
+
+        Returns:
+            Vectorized corpus
+        """
+
         input = self.__check_input(input)
 
         self.fit(input=input, ignore_stopwords=ignore_stopwords)
