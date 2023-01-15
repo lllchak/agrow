2f3f0a631b6e1d44ab3f454c357e8ae0dd69343f
lllchak <mediumchak@yandex.ru>
2023-01-15 14:57:06 +0300

    added TfidfVectorizer docs & fixed its __repr__ method

--- vectorizers/tfidf.py
+++ vectorizers/tfidf.py
@@ -16,13 +16,18 @@ class TfidfVectorizer(BaseVectorizer):
     fit(...) and tranform(...) or fit_tranform(...) methods (should be overwritten).
     """
 
+    """
+    TfidfVectorizer custom attributes
+    """
     __slots__ = [
         "vidf_"
     ]
 
     def __repr__(self) -> str:
-        return "{}()".format(
-            self.__class__.__name__
+        return "{}(size={}, stopwords={})".format(
+            self.__class__.__name__,
+            len(self.corpus_),
+            self.stopwords_
         )
 
     def fit(
@@ -31,6 +36,19 @@ class TfidfVectorizer(BaseVectorizer):
         ignore_stopwords: bool = True,
         tokenizer: dtypes.Any = PunctTokenizer
     ) -> None:
+        """
+        You can find more complete docs at ./base.py
+
+        Fitting on a given corpus method.
+
+        Args:
+            input (CorpusInput)     : Corpus to fit with
+            ignore_stopwords (bool) : If ignore corpus stopwords or not flag
+
+        Returns:
+            None (only creates corpus vocabulary)
+        """
+
         self._check_input(input)
 
         self._ccorpus(
@@ -39,8 +57,24 @@ class TfidfVectorizer(BaseVectorizer):
             tokenizer=tokenizer
         )
 
-    def transform(self, input: CorpusInput) -> VectorizedOutput:
         self.__idf(input=input)
+
+    def transform(self, input: CorpusInput) -> VectorizedOutput:
+        """
+        You can find more complete docs at ./base.py
+
+        Tranforming given corpus method.
+
+        Args:
+            input (CorpusInput) : Corpus to be vectorized
+
+        Returns:
+            Vectorized corpus
+        """
+
+        input = self._check_input(input)
+
+        return [self.__trsent(sent) for sent in input]
 
     def fit_transform(
         self,
@@ -48,12 +82,45 @@ class TfidfVectorizer(BaseVectorizer):
         ignore_stopwords: bool = True,
         tokenizer: dtypes.Any = PunctTokenizer
     ) -> VectorizedOutput:
-        return ["string"]
+        """
+        You can find more complete docs at ./base.py
+
+        Fitting and tranforming corpus wrapper method
+
+        Args:
+            input (CorpusInput)     : Corpus to fit with and which to transform after
+            ignore_stopwords (bool) : If ignore corpus stopwords or not flag
+
+        Returns:
+            Vectorized corpus
+        """
+
+        input = self._check_input(input)
+
+        self.fit(
+            input=input,
+            ignore_stopwords=ignore_stopwords,
+            tokenizer=tokenizer
+        )
+
+        return self.transform(input)
 
     def __idf(
         self,
         input: CorpusInput,
     ) -> None:
+        """
+        Finding corpus elements IDF (Inverse document frequency) method.
+        Calculates IDF value for each of the corpus elements by searching
+        element occuracy in all contexts.
+
+        Args:
+            input (CorpusInput) : Corpus to run through
+
+        Returns:
+            None (only stores IDF value for each corpus element in vidf_ attribute (dict))
+        """
+
         self.vidf_: dtypes.Dict[str, float] = {}
 
         if not self.corpus_: 
@@ -73,3 +140,33 @@ class TfidfVectorizer(BaseVectorizer):
                     )
                     cnt += word == tok
             self.vidf_[word] = 1 + math.log((1 + len(input)) / (1 + cnt))
+
+    def __trsent(self, input: str) -> dtypes.List[str]:
+        """
+        Transforming single given string (sentence/context) method. Calls by tranform(...)
+        to vectorize full corpus.
+
+        Args:
+            input (str) : String (sentence/context) to be vectorized
+
+        Returns:
+            Vectorized string (sentence/context)
+        """
+
+        input_tokens: dtypes.List[str] = self.tk_().tokenize(input)
+        res_vec = [0] * len(self.corpus_)
+
+        print(input_tokens)
+
+        for idx, tok in enumerate(input_tokens):
+            # In case we can't process tokens like "end." and "end" at the end 
+            # of string (sentence/context) like different tokens.
+            tok = self._preprocess_tok(tok=tok, tokens=input_tokens, curr_idx=idx)
+            if tok in self.corpus_:
+                res_vec[self.indices_[tok]] += 1
+
+        for tok in input_tokens:
+            if tok in self.corpus_:
+                res_vec[self.indices_[tok]] /= self.vidf_[tok]
+
+        return res_vec
