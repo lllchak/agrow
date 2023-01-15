a184eb70f6c3459281202f23f1b47c5d0b90290c
lllchak <mediumchak@yandex.ru>
2023-01-15 17:41:11 +0300

    removed OneHotVectorizer

--- vectorizers/__init__.py
+++ vectorizers/__init__.py
@@ -3,11 +3,9 @@ import dtypes
 from .base import BaseVectorizer
 from .count import CountVectorizer
 from .tfidf import TfidfVectorizer
-from .onehot import OneHotVectorizer
 
 __all__ = [
     "BaseVectorizer",
     "CountVectorizer",
     "TfidfVectorizer",
-    "OneHotVectorizer",
 ]
