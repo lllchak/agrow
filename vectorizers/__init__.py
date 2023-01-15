8ec42f26fde360f1636e625b38c305d15a9b217a
lllchak <mediumchak@yandex.ru>
2023-01-15 15:17:15 +0300

    OneHotVectorizer

--- vectorizers/__init__.py
+++ vectorizers/__init__.py
@@ -3,9 +3,11 @@ import dtypes
 from .base import BaseVectorizer
 from .count import CountVectorizer
 from .tfidf import TfidfVectorizer
+from .onehot import OneHotVectorizer
 
 __all__ = [
     "BaseVectorizer",
     "CountVectorizer",
-    "TfidfVectorizer"
+    "TfidfVectorizer",
+    "OneHotVectorizer",
 ]
