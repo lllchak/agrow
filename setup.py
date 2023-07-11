79ab5eb008fa1b10eeb2aa164da7fa34e2f2f1bb
lllchak <mediumchak@yandex.ru>
2023-07-11 22:06:43 +0300

    master - refactored setup.py

--- setup.py
+++ setup.py
@@ -1,41 +1,66 @@
-from setuptools import setup, Extension
-from Cython.Distutils import build_ext
+import os
+from pathlib import Path
+from enum import Enum
+from typing import List
 
-def make_package_from_path(path: str) -> str:
-    return '.'.join(path.split('/'))
+from Cython.Distutils import build_ext
+from setuptools import (
+    setup,
+    Extension
+)
+
 
 AUTHOR = "Pavel Lyulchak"
 EMAIL = "mediumchak@yandex.ru"
-
+LICENSE = "MIT Licence"
 NAME = "agrow"
 VERSION = "0.1"
-DESCR = "A small template project that shows how to wrap C/C++ code into python using Cython"
+# keep in sync with github description
+DESCR = "Python framework for solving math & NLP (Natural Language Processing) tasks"
+# add PyPi url after package deploy
 URL = ""
 
-with open("requirements.txt") as file:
-    reqs = [line.rstrip() for line in file]
+C_SOURCE_EXTENSION = ".c"
+WRAPPER_EXTENSION = ".pyx"
 
-REQUIRES = reqs
 
-LICENSE = "MIT Licence"
+def get_requirements() -> List[str]:
+    with open("requirements.txt") as file:
+        return [line.rstrip() for line in file]
 
-MATH_FUNC_DIR = "agrow/math/func"
-PACKAGES = [
-    make_package_from_path(MATH_FUNC_DIR)
-]
 
-EXTENSIONS = [
-    # agrow.math.func
-    Extension(
-        '.'.join(MATH_FUNC_DIR.split('/')) + ".func", 
-        [MATH_FUNC_DIR + "/core/func.c", MATH_FUNC_DIR + "/func.pyx"]
+def generate_extension(path: Path, package: str) -> Exception:
+    module_name: str = get_module_name(package)
+
+    return Extension(
+        package + f".{module_name}",
+        [
+            os.path.join(path, f"core/{module_name}{C_SOURCE_EXTENSION}"),
+            os.path.join(path, f"{module_name}{WRAPPER_EXTENSION}")
+        ]
     )
-]
+
+
+def get_module_name(package: str) -> str:
+    return package.split('.')[-1]
+
+
+class BuildPackages(Enum):
+    MATH_FUNC = ("agrow.math.func", Path("agrow/math/func"))  # agrow.math.func
+
+    @classmethod
+    def values(cls):
+        return list(map(lambda path: path.value, cls))
+
+
+PACKAGES_INFO = [package_info for package_info in BuildPackages.values()]
+EXTENSIONS = [generate_extension(path, package) for package, path in PACKAGES_INFO]
+
 
 if __name__ == "__main__":
     setup(
-        install_requires=REQUIRES,
-        packages=PACKAGES,
+        install_requires=get_requirements(),
+        packages=[p for p, _, in PACKAGES_INFO],
         zip_safe=False,
         name=NAME,
         version=VERSION,
