f597b12e2be57649d5394011eec865c0844f1e78
lllchak <mediumchak@yandex.ru>
2023-07-19 23:24:00 +0300

    add pyproject.toml config to build & sanitized trash script

--- .scripts/sanitize.sh
+++ .scripts/sanitize.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 BUILD="build"
+DISTRIBUTION="dist"
 BUILD_INFO="agrow.egg-info"
 LIBRARIES="*.so"
 SRC="agrow"
 
-rm -rf $BUILD $BUILD_INFO 
+rm -rf $DISTRIBUTION $BUILD $BUILD_INFO
 find $SRC -name $LIBRARIES -exec rm -f {} \;
--- .scripts/test_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo Tokenizers tests
-python tecode/tokenizers/tokenizers_tests.py
-
-echo Vectorizers tests
-python tecode/vectorizers/vectorizers_tests.py
